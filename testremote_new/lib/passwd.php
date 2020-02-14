7524dcff92fd7cf1cc18e8efbe1180b096522631
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2020-02-14 21:56:30 +0100

    passwd juiste instellingen

--- testremote_new/lib/passwd.php
+++ testremote_new/lib/passwd.php
@@ -2,8 +2,8 @@
 function GetConnectionData()
 {
     return array( "dbhost" => "het_juiste_host_adres",
-                            "dbname" => "de_database_naam",
-                            "dbuser" => "de_database_user",
-                            "dbpasswd" => "het_juiste_wachtwoord" ) ;
+                            "dbname" => "steden",
+                            "dbuser" => "root",
+                            "dbpasswd" => "mysql" ) ;
 }
 ?>
