bd95ee7b7b0d1041f8a4de9c9e59b512eed15fcd
gdm-1718-annepeet2 <annepeet2@student.arteveldehs.be>
2020-02-29 14:07:49 +0100

    UploadService

--- testremote_new/Service/UploadService.php
+++ testremote_new/Service/UploadService.php
@@ -5,18 +5,17 @@ class UploadService
 {
     public function ProfielUpload()
     {
-        if ( isset($_POST["submit"]) == "Opladen" )
-        {
+        if (isset($_POST["submit"]) == "Opladen") {
             //$target_dir = de map waar de afbeeldingen uiteindelijk moet komen
-            $target_dir = "../img/";                                                          //de map waar de afbeelding uiteindelijk moet komen; relatief pad tov huidig script
-            $max_size = 5000000;                                                           //maximum grootte in bytes
+            $target_dir = "../img/"; //de map waar de afbeelding uiteindelijk moet komen; relatief pad tov huidig script
+            $max_size = 5000000;     //maximum grootte in bytes
 
             $images = array();
 
             //pasfoto, eid_voorzijde en eid_achterzijde overlopen
-            foreach ( $_FILES as $inputname => $fileobject )   //overloop alle bestanden in $_FILES
+            foreach ($_FILES as $inputname => $fileobject)   //overloop alle bestanden in $_FILES
             {
-                $tmp_name= $fileobject["tmp_name"];
+                $tmp_name = $fileobject["tmp_name"];
                 $originele_naam = $fileobject["name"];
                 $size = $fileobject["size"];
                 $extensie = pathinfo($originele_naam, PATHINFO_EXTENSION);
@@ -25,26 +24,23 @@ class UploadService
 
                 //CONTROLES
                 $max_size = 20000000; //maximum grootte in bytes
-                $allowed_extensions = [ "jpeg", "jpg", "png", "gif" ]; //toegelaten bestandsextensies
+                $allowed_extensions = ["jpeg", "jpg", "png", "gif"]; //toegelaten bestandsextensies
                 $cancel = false;
 
                 //grootte
-                if ( $size > $max_size )
-                {
+                if ($size > $max_size) {
                     print "Bestand " . $originele_naam . " is te groot (" . $size . " bytes). Maximum $max_size bytes!<br>";
                     $cancel = true;
                 }
 
                 //toegelaten extensies
-                if ( ! in_array( pathinfo($originele_naam, PATHINFO_EXTENSION), $allowed_extensions ))
-                {
+                if (!in_array(pathinfo($originele_naam, PATHINFO_EXTENSION), $allowed_extensions)) {
                     print "Bestand " . $originele_naam . ": verkeerde bestandsextensie!<br>";
                     $cancel = true;
                 }
 
                 //is het bestand wel echt een afbeelding?
-                if ( getimagesize($tmp_name) === false)
-                {
+                if (getimagesize($tmp_name) === false) {
                     print "Bestand " . $originele_naam . " is niet echt een afbeelding!<br>";
                     $cancel = true;
                 }
@@ -58,10 +54,8 @@ class UploadService
                 }
                 */
 
-                if ( ! $cancel )
-                {
-                    switch ( $inputname )
-                    {
+                if (!$cancel) {
+                    switch ($inputname) {
                         case "pasfoto":
                             $target = "pasfoto_" . $_SESSION["usr"]["usr_id"] . "." . $extensie;
                             $images[] = "usr_pasfoto='" . $target . "'";
@@ -81,20 +75,18 @@ class UploadService
                     //bestand verplaatsen naar definitieve locatie
                     print "Moving " . $inputname . " to " . $target . "<br>";
 
-                    if ( move_uploaded_file( $tmp_name, $target))
-                    {
+                    if (move_uploaded_file($tmp_name, $target)) {
                         print "Bestand $originele_naam opgeladen<br>";
-                    }
-                    else print "Sorry, there was an unexpected error uploading file " . $originele_naam . "<br>";
+                    } else print "Sorry, there was an unexpected error uploading file " . $originele_naam . "<br>";
                 }
             }
 
             //de afbeeldingen opslaan in het gebruikersprofiel
-            $sql = "update users SET " . implode("," , $images) . " where usr_id=" . $_SESSION["usr"]["usr_id"];
+            $sql = "update users SET " . implode(",", $images) . " where usr_id=" . $_SESSION["usr"]["usr_id"];
             ExecuteSQL($sql);
 
             //eventueel een redirect naar de profielpagina
 
         }
-
+    }
 }
