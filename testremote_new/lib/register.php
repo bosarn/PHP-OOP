a4d182baf7a415e625c82f2852827d42206fbecf
bosarn <58468105+bosarn@users.noreply.github.com>
2020-03-01 20:54:56 +0100

    Merge branch 'master' of C:\Users\Dave\PhpstormProjects\PHP-OOP with conflicts.

--- testremote_new/lib/register.php
+++ testremote_new/lib/register.php
@@ -1,5 +1,19 @@
 <?php
 $register_form = true;
 require_once "autoload.php";
+
+$User = $_POST;
+$UserService = new UserService();
 $UserService->PushRegister();
 
+//$formname = $_POST["formname"];
+//$tablename = $_POST["tablename"];
+//$pkey = $_POST["pkey"];
+//
+//if ( $formname == "registration_form" AND $_POST['register-but'] == "Register" )
+//{
+//    $UserService = new UserService();
+//    $User = new User();
+//    $UserService->ValidatePostedUserData($User);
+//    $UserService->RegisterUser($User);
+//}
---
+++ testremote_new/lib/view_functions.php
@@ -0,0 +1,41 @@
+<?php
+
+function PrintNavBar()
+{
+    //navbar items ophalen
+    $data = GetData("select * from menu order by men_order");
+
+    $laatste_deel_url = basename($_SERVER['SCRIPT_NAME']);
+
+    //aan de juiste datarij, de sleutels 'active' en 'sr-only' toevoegen
+    foreach( $data as $r => $row )
+    {
+        if ( $laatste_deel_url == $data[$r]['men_destination'] )
+        {
+            $data[$r]['active'] = 'active';
+            $data[$r]['sr_only'] = '<span class="sr-only">(current)</span>';
+        }
+        else
+        {
+            $data[$r]['active'] = '';
+            $data[$r]['sr_only'] = '';
+        }
+    }
+
+    //template voor 1 item samenvoegen met data voor items
+    $TemplateLoader = new TemplateLoader();
+
+    $template_navbar_item = $TemplateLoader->LoadTemplate("navbar_item");
+    $navbar_items = ReplaceContent($data, $template_navbar_item);
+
+    //navbar template samenvoegen met resultaat ($navbar_items)
+    $data = array( "navbar_items" => $navbar_items ) ;
+    $template_navbar = $TemplateLoader->LoadTemplate("navbar");
+    print ReplaceContentOneRow($data, $template_navbar);
+}
+
+
+
+
+
+/* Deze functie voegt data en template samen en print het resultaat */
